84b11c32ea960e2327ada6b765c4f42b5d0e3238
azure1307 <azure1307@gmail.com>
2025-02-07 22:17:41 +0900

    0207 MVC3 3일차 끝

--- sp5-chap13/src/main/java/config/MvcConfig.java
+++ sp5-chap13/src/main/java/config/MvcConfig.java
@@ -6,16 +6,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import Interceptor.AuthCheckInterceptor;
 
 @Configuration
 @EnableWebMvc
 public class MvcConfig implements WebMvcConfigurer {
 
 	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+	public void configureDefaultServletHandling(
+					DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
 	}
 	
@@ -27,6 +31,18 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/main").setViewName("main");
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authCheckInterceptor())
+				.addPathPatterns("/edit/**")
+				.excludePathPatterns("/edit/help/**");
+	}
+	
+	@Bean
+	public AuthCheckInterceptor authCheckInterceptor() {
+		return new AuthCheckInterceptor();
 	}
 	
 	@Bean
--- sp5-chap13/src/main/java/controller/LoginController.java
+++ sp5-chap13/src/main/java/controller/LoginController.java
@@ -1,9 +1,12 @@
 package controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,23 +25,41 @@ public class LoginController {
 	}
 	
 	@GetMapping
-	public String form(LoginCommand loginCommand) {
+	public String form(LoginCommand loginCommand,
+			@CookieValue(value = "REMEMBER", required = false) Cookie rCookie ) {
+		if (rCookie != null) {
+			loginCommand.setEmail(rCookie.getValue());
+			loginCommand.setRememberEmail(true);
+		}
 		return "login/loginForm";
 	}
 	
 	@PostMapping
-	public String submit(LoginCommand loginCommand, Errors errors, HttpSession session) {
+	public String submit(
+			LoginCommand loginCommand, Errors errors, HttpSession session,
+			HttpServletResponse response) {
+		// 1. 유효성 검사
 		new LoginCommandValidator().validate(loginCommand, errors);
 		if (errors.hasErrors()) {
 			return "login/loginForm";
 		}
 		try {
+			// 2. 인증 절차 후 세션에 로그인정보 저장
 			AuthInfo authInfo = authService.authenticate(
 													loginCommand.getEmail(),
 													loginCommand.getPassword());
 			
-			// TODO 세션에 authInfo 저장해야 함
 			session.setAttribute("authInfo", authInfo);
+			
+			// 3. 쿠키 생성 후 response에 저장
+			Cookie rememberCookie = new Cookie("REMEMBER", loginCommand.getEmail());
+			rememberCookie.setPath("/");
+			if (loginCommand.isRememberEmail()) { // '이메일 기억하기' 받아온 값이 true면
+				rememberCookie.setMaxAge(60 * 60 * 24 * 30); // 30일 유지
+			} else {
+				rememberCookie.setMaxAge(0); // 바로 삭제
+			}
+			response.addCookie(rememberCookie);
 			
 			return "login/loginSuccess";
 		} catch (WrongIdPasswordException e) {
