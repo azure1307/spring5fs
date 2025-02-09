c7cb91574be0118552ee47649868bce6dede269e
azure1307 <azure1307@gmail.com>
2025-02-09 21:03:14 +0900

    0209 MVC4 2일차 끝, 간단한 웹 어플리케이션의 구조 1일차 끝

--- sp5-chap14/src/main/java/config/ControllerConfig.java
+++ sp5-chap14/src/main/java/config/ControllerConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import controller.ChangePwdController;
 import controller.LoginController;
 import controller.LogoutController;
+import controller.MemberDetailController;
 import controller.MemberListController;
 import controller.RegisterController;
 import spring.AuthService;
@@ -67,4 +68,12 @@ public class ControllerConfig {
 		controller.setMemberDao(memberDao);
 		return controller;
 	}
+	
+	@Bean
+	public MemberDetailController memberDetailController() {
+		MemberDetailController controller = new MemberDetailController();
+		controller.setMemberDao(memberDao);
+		return controller;
+	}
+	
 }
--- sp5-chap14/src/main/java/spring/MemberDao.java
+++ sp5-chap14/src/main/java/spring/MemberDao.java
@@ -19,6 +19,20 @@ import org.springframework.jdbc.support.KeyHolder;
 public class MemberDao {
 	
 	private JdbcTemplate jdbcTemplate;
+	private RowMapper<Member> memRowMapper =
+					new RowMapper<Member>() {
+						
+						@Override
+						public Member mapRow(ResultSet rs, int rowNum)
+										throws SQLException {
+							Member member = new Member(rs.getString("EMAIL"),
+											rs.getString("PASSWORD"),
+											rs.getString("NAME"),
+											rs.getTimestamp("REGDATE").toLocalDateTime());
+							member.setId(rs.getLong("ID"));
+							return member;
+						}
+					};
 	
 	public MemberDao(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
@@ -27,18 +41,7 @@ public class MemberDao {
 	public Member selectByEmail(String email) {
 		List<Member> results = jdbcTemplate.query(
 											"select * from MEMBER where EMAIL = ?",
-											new RowMapper<Member>() {
-												@Override
-												public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
-													Member member = new Member(
-															rs.getString("EMAIL"),
-															rs.getString("PASSWORD"),
-															rs.getString("NAME"),
-															rs.getTimestamp("REGDATE").toLocalDateTime());
-													member.setId(rs.getLong("ID"));
-													return member;
-												}
-											}, email);
+											memRowMapper, email);
 		return results.isEmpty() ? null : results.get(0);
 	}
 	
@@ -73,15 +76,7 @@ public class MemberDao {
 	
 	public List<Member> selectAll() {
 		List<Member> results = jdbcTemplate.query("select * from MEMBER",
-											(ResultSet rs, int rowNum) -> {
-												Member member = new Member(
-														rs.getString("EMAIL"),
-														rs.getString("PASSWORD"),
-														rs.getString("NAME"),
-														rs.getTimestamp("REGDATE").toLocalDateTime());
-												member.setId(rs.getLong("ID"));
-												return member;
-											});
+											memRowMapper);
 		return results;
 	}
 	
@@ -95,21 +90,16 @@ public class MemberDao {
 		List<Member> results = jdbcTemplate.query(
 						"select * from MEMBER where REGDATE between ? and ? " +
 							"order by REGDATE desc",
-						new RowMapper<Member>() {
-							@Override
-							public Member mapRow(ResultSet rs, int rowNum)
-									throws SQLException {
-								Member member = new Member(
-									rs.getString("EMAIL"),
-									rs.getString("PASSWORD"),
-									rs.getString("NAME"),
-									rs.getTimestamp("REGDATE").toLocalDateTime());
-								member.setId(rs.getLong("ID"));
-								return member;
-							}
-						},
+						memRowMapper,
 						from, to);
 		return results;
+	}
+	
+	public Member selectById(Long memId) {
+		List<Member> results = jdbcTemplate.query(
+						"select * from MEMBER where ID = ?",
+						memRowMapper, memId);
+		return results.isEmpty() ? null : results.get(0);
 	}
 
 }
