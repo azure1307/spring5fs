97b59ff4a092546c91b906b0268582ffcdd6638f
azure1307 <azure1307@gmail.com>
2025-02-12 23:39:53 +0900

    0212 프로필과 프로퍼티 파일 1일차 끝

---
+++ sp5-chap17/src/main/java/controller/ErrorResponse.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class ErrorResponse {
+	private String message;
+
+	public ErrorResponse(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+}
